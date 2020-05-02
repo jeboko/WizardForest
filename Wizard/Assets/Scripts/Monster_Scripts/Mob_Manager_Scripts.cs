3d491b8b11ddf65fbe555c74b38e68efb6c8ff9a
Nebula16 <tjddns1016@gmail.com>
2020-05-02 19:13:25 +0900

    Add Monster_Manager
    Monster_Manager를 만들었습니다.
    해당 Object에서 Mob_info로 리스트를 만들고 각 몬스터의 정보를 입력해주면 자동으로 몬스터 프리팝에 정보가 업데이트 되는 형식으로 코드를 만들었습니다.

---
+++ Wizard/Assets/Scripts/Monster_Scripts/Mob_Manager_Scripts.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mob_Manager_Scripts : MonoBehaviour
+{
+    // 몹 리스트를 만든다.
+    public List<Mob_info> deck = new List<Mob_info>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

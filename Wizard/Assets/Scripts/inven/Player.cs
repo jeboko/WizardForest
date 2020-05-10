8d054be3a3f5646ec1ada6821d9d306f718e89c3
jeongjunhyeop <lost034@naver.com>
2020-05-10 14:44:51 +0900

    인벤토리 시스템 및 건설
    1. 아이템 줍기
    2. 인벤토리에 수량 변화 적용
    3. 임의로 나무를 쌓은 오브젝트를 건설할 수 있게 하였음
    4. WASD 조작, Q건설 E 인벤토리 열기
    SPACE 나무 OR 돌 줍기

---
+++ Wizard/Assets/Scripts/inven/Player.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+	public float speed = 1.0F;
+	public int rock_count = 0;
+	public int wood_count = 0;
+
+	public Text countRock;
+	public Text countWood;
+
+	public GameObject baricate;
+
+	private void OnTriggerStay(Collider col)
+{
+		if(Input.GetKey(KeyCode.Space))
+		{
+			if (col.gameObject.tag == "rock")
+			{
+				Destroy(col.gameObject);
+				rock_count++;
+			}
+			else if (col.gameObject.tag == "wood")
+			{
+				Destroy(col.gameObject);
+				wood_count++;
+			}
+		}
+}	
+	void Start()
+	{
+		
+	}
+
+
+	void Update()
+	{
+		// 재료 카운트 텍스트에 입력
+		countRock.text = "바위 X " + rock_count;
+		countWood.text = "목재 X " + wood_count;
+
+		if (Input.GetKey(KeyCode.D))
+			transform.Translate(Vector3.right * speed * Time.deltaTime);
+
+
+		if (Input.GetKey(KeyCode.A))
+			transform.Translate(Vector3.left * speed * Time.deltaTime);
+
+
+		if (Input.GetKey(KeyCode.W))
+			transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+
+		if (Input.GetKey(KeyCode.S))
+			transform.Translate(Vector3.back * speed * Time.deltaTime);
+
+		if (Input.GetKeyDown(KeyCode.Q))
+		{
+			Instantiate(baricate, transform.position, Quaternion.identity);
+			rock_count--;
+			wood_count--;
+		}
+	}
+}
---
+++ Wizard/Assets/Scripts/inven/gamemanager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gamemanager : MonoBehaviour
+{
+    public GameObject inven;
+    private bool inven_state = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (inven_state == false)
+            {
+                inven.SetActive(true);
+                inven_state = true;
+            }
+            else if (inven_state == true)
+            {
+                inven.SetActive(false);
+                inven_state = false;
+            }
+        }
+    }
+}
+

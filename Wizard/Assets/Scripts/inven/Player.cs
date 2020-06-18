b0f6e6f9d9ecd81ebfd843159a3472e33516c71a
jeongjunhyeop <lost034@naver.com>
2020-06-19 00:02:23 +0900

    건설 시스템 95% / 작업물 합칠 준비
    1. 건설 시스템 마무리 단계
    코드 합치기 전 충돌 방지를 위한 작업
    2. 건설 오브젝트의 실제 기능
    구현용 코드 추가

--- Wizard/Assets/Scripts/inven/Player.cs
+++ Wizard/Assets/Scripts/inven/Player.cs
@@ -4,26 +4,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+//player의 건설 관련 능력 스크립트임
+
 public class Player : MonoBehaviour
 {
 	public float speed = 1.0F;
 	public int rock_count = 0;
 	public int wood_count = 0;
 
+    public int red_count = 0;
+    public int yellow_count = 0;
+    public int blue_count = 0;
+
     public GameObject build_comp_Bt;
     //건물 최종 설치 버튼 활성화 오브젝트
 
     public Text countRock;
 	public Text countWood;
+    public Text countRed;
+    public Text countYellow;
+    public Text countBlue;
 
-	public GameObject baricate;
+    public GameObject baricate;
     public GameObject fire_b;
     //생성할 건설 오브젝트
 
     public GameObject build_checker;
-	private GameObject tempo;
+	public GameObject tempo;
     private GameObject tempo2;
+    private GameObject NULL1;
+
     //플레이어가 임시로 설치할 건물을 들고있을 때 그 건물을 저장할 오브젝트 변수
+    // NULL1은 건설 오브젝트를 취소할 때 들고있는 오브젝트를 취소하기 위한 공백 변수
 
     private bool building_state = false;
 	//플레이어가 이미 건설 준비중이면 새로운 오브젝트를 손에 들지 못하게 하기 위한 불 변수
@@ -42,7 +54,22 @@ public class Player : MonoBehaviour
 				Destroy(col.gameObject);
 				wood_count++;
 			}
-		}
+            else if (col.gameObject.tag == "B_flower")
+            {
+                Destroy(col.gameObject);
+                blue_count++;
+            }
+            else if (col.gameObject.tag == "Y_flower")
+            {
+                Destroy(col.gameObject);
+                yellow_count++;
+            }
+            else if (col.gameObject.tag == "R_flower")
+            {
+                Destroy(col.gameObject);
+                red_count++;
+            }
+        }
     }	
 
 
@@ -52,8 +79,11 @@ public class Player : MonoBehaviour
 		// 재료 카운트 텍스트에 입력
 		countRock.text = "바위 X " + rock_count;
 		countWood.text = "목재 X " + wood_count;
+        countRed.text = "빨간 꽃 X " + red_count;
+        countYellow.text = "노란 꽃 X " + yellow_count;
+        countBlue.text = "파란 꽃 X " + blue_count;
 
-		if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D))
 			transform.Translate(Vector3.right * speed * Time.deltaTime);
 
 
@@ -83,21 +113,21 @@ public class Player : MonoBehaviour
         {
             if (tempo.tag == "baricate")
             {
-                tempo2 = Instantiate(baricate, build_checker.transform.position, Quaternion.identity);
-                Destroy(tempo);
-                rock_count--;
-                wood_count--;
-                tempo2.GetComponent<BuildObj>().state = true;
-                building_state = false;
+                    tempo2 = Instantiate(baricate, build_checker.transform.position, gameObject.transform.rotation);
+                    Destroy(tempo);
+                    rock_count--;
+                    wood_count--;
+                    tempo2.GetComponent<BuildObj>().state = true;
+                    building_state = false;
+           
             }
 
             else if(tempo.tag == "fire_barrel")
             {
 
-                tempo2 = Instantiate(fire_b, build_checker.transform.position, Quaternion.identity);
+                tempo2 = Instantiate(fire_b, build_checker.transform.position, gameObject.transform.rotation);
                 Destroy(tempo);
-                rock_count--;
-                wood_count--;
+                red_count--;
                 tempo2.GetComponent<BuildObj>().state = true;
                 building_state = false;
             }
@@ -110,23 +140,33 @@ public class Player : MonoBehaviour
     //건설이 가능할 때 최종설치하는 함수
 
 
-	public void Create_B()// 건설 준비 버튼(바리케이드)
-	{
-		if (building_state == false) // 설치 준비
-		{
-			building_state = true;
-			Instantiate(baricate, build_checker.transform.position, Quaternion.identity).transform.parent = build_checker.transform;
-			tempo = transform.Find("build_checker").transform.Find("sample_build(Clone)").gameObject;
-		}
-	}
+    public void Create_B()// 건설 준비 버튼(바리케이드)
+    {
+        if (building_state == false) // 설치 준비
+        {
+            building_state = true;
+            Instantiate(baricate, build_checker.transform.position, gameObject.transform.rotation).transform.parent = build_checker.transform;
+            tempo = transform.Find("build_checker").transform.Find("sample_build(Clone)").gameObject;
+
+        }
+    }
+
 
     public void create_F()// 건설 준비 버튼(불꽃 통)
     {
+       
         if (building_state == false) // 설치 준비
         {
             building_state = true;
             Instantiate(fire_b, build_checker.transform.position, Quaternion.identity).transform.parent = build_checker.transform;
             tempo = transform.Find("build_checker").transform.Find("fire_barrel(Clone)").gameObject;
+            
         }
+    }
+
+    public void cancel_B()
+    {
+        Destroy(tempo);
+        building_state = false;
     }
 }
--- Wizard/Assets/Scripts/inven/buildButtonSc.cs
+++ Wizard/Assets/Scripts/inven/buildButtonSc.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class buildButtonSc : MonoBehaviour
 {
-   
+
+    public GameObject Error_text;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,22 +18,43 @@ public class buildButtonSc : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (GameObject.Find("Player").GetComponent<Player>().rock_count == 0 || GameObject.Find("Player").GetComponent<Player>().wood_count == 0)
+        {
+            GameObject.Find("baricate_B").GetComponent<Button>().interactable = false;
+        }
+        else if (GameObject.Find("Player").GetComponent<Player>().rock_count >= 1 && GameObject.Find("Player").GetComponent<Player>().wood_count >= 1)
+        {
+            GameObject.Find("baricate_B").GetComponent<Button>().interactable = true;
+        }
 
+        if (GameObject.Find("Player").GetComponent<Player>().red_count == 0)
+        {
+            GameObject.Find("firebarrel_B").GetComponent<Button>().interactable = false;
+        }
+        else if (GameObject.Find("Player").GetComponent<Player>().red_count >= 1)
+        {
+            GameObject.Find("firebarrel_B").GetComponent<Button>().interactable = true;
+        }
     }
 
     public void createBaricate()
-    {
-        GameObject.Find("Player").GetComponent<Player>().Create_B();
+    {    
+            GameObject.Find("Player").GetComponent<Player>().Create_B();
     } // 설치 UI의 바리케이트 버튼 함수
 
     public void createFireBarrel()
     {
         GameObject.Find("Player").GetComponent<Player>().create_F();
-    } // 설치 UI의 바리케이트 버튼 함수
+    } // 설치 UI의 불꽃통 버튼 함수
 
     public void complete_Build()
     {
         GameObject.Find("Player").GetComponent<Player>().build_complete();
     } // 설치 가능상태일 때 설치를 완료하는 함수
+
+    public void cancel_Build()
+    {
+        GameObject.Find("Player").GetComponent<Player>().cancel_B();
+    }// 건설 준비상태를 취소하는 버튼
 
 }
